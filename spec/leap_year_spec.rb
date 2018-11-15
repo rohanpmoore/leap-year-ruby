dc05a7db4c9faa4f1aa35272215d4a2adb879b14
Kristin Brewer_Lowe and Rohan Moore <kristin@>
2018-11-15 13:36:07 -0800

    Add spec 2; fails

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -5,4 +5,7 @@ describe('leap_year') do
   it('will return true for a year divisible by 4') do
     expect(leap_year(2004)).to(eq(true))
   end
+  it('will return false for years divisible by 100') do
+    expect(leap_year(1900)).to(eq(false))
+  end
 end
